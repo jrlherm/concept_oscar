b7b92f249a0b016bfe3649dad633bb449b7ff2ca
Long <clmt.long@gmail.com>
2016-04-12 09:59:24 +0200

    Test API

--- model/DbRepository.php
+++ model/DbRepository.php
@@ -19,4 +19,15 @@ class dbRepository
         $this->pdo = $pdo;
     }
 
+    public function test()
+    {
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, 'https://api.themoviedb.org/3/movie/550?api_key=7293cd7321f0ae4ca7110422285f7f35');
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($curl);
+        curl_close($curl);
+
+        return json_decode($result); // Json decode
+    }
+
 }
