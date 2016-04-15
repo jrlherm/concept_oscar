f91b961008ae0d196009080d2be9a78279334739
Long <clmt.long@gmail.com>
2016-04-15 13:59:29 +0200

    Final commit of the dead

--- view/partial/footer.php
+++ view/partial/footer.php
@@ -13,6 +13,7 @@
 <script type="text/javascript" src="public/js/lib/jquery-2.2.2.min.js"></script>
 <script type="text/javascript" src="public/js/lib/jit.js"></script>
 <script type="text/javascript" src="public/js/lib/owl.carousel.min.js"></script>
+<script type="text/javascript" src="public/js/lib/gcharts.js"></script>
 <script type="text/javascript" src="public/js/app/main.js"></script>
 </body>
 </html>
